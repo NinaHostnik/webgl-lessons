0bd6431437bd4c661844403c5f357598196faaea
NinaHostnik <44391618+NinaHostnik@users.noreply.github.com>
2018-10-23 08:43:39 +0200

    Update 00-introduction.js

--- scripts/00-introduction.js
+++ scripts/00-introduction.js
@@ -16,7 +16,7 @@ function start() {
 
   // Only continue if WebGL is available and working
   if (gl) {
-    gl.clearColor(0.0, 0.0, 0.0, 1.0);                      // Set clear color to black, fully opaque
+    gl.clearColor(1.0, 1.0, 0.0, 1.0);                      // Set clear color to yellow, fully opaque
     gl.clearDepth(1.0);                                     // Clear everything
     gl.enable(gl.DEPTH_TEST);                               // Enable depth testing
     gl.depthFunc(gl.LEQUAL);                                // Near things obscure far things
